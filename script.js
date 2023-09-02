6ffe21ad7be92e555c9f11247c28a29675362844
Shoibuuy <142968488+Shoibuuy@users.noreply.github.com>
2023-09-02 16:05:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// overlay menu
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+}
+
+
+//sumbmission
+
+function alertc() {
+    alert("Your Reservation has been confirmed");
+}
